1b0430ca883dd7901a70b1efd0e6a79e18a486f5
sattucoder23 <tripathisatyam367@gmail.com>
2024-09-30 15:17:56 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
     
+    let matchingBracket = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+   
+    for (let char of s) {
+       
+        if (matchingBracket[char]) {
+            
+            let topElement = stack.length ? stack.pop() : '#';
+            if (topElement !== matchingBracket[char]) {
+                return false;
+            }
+        } else {
+            
+            stack.push(char);
+        }
+    }
+
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
